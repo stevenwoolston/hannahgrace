538ee093d6d8aca4b84e451f2ad7ad3ed27f009b
Woolston Web Design <steven.woolston@gmail.com>
2021-05-29 20:55:26 +1000

    WIP Calendar

--- app/public/wp-content/themes/hannahgracebeauty/functions.php
+++ app/public/wp-content/themes/hannahgracebeauty/functions.php
@@ -18,23 +18,21 @@ function maintenance_redirect(){
 // add_action('init', 'maintenance_redirect');
 
 function wwd_load_scripts() {
-    wp_deregister_script('jquery');
-    wp_register_script('jquery', get_template_directory_uri() . '/js/jquery-3.3.1.min.js', false, '3.3.1', true);
 
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto&family=Varela+Round', array(), '1.0', 'all');
 
     wp_enqueue_style('icon-fonts', get_template_directory_uri() . '/css/line-awesome.min.css', array(), '1.0.0', 'all');
-    wp_enqueue_style('lite-yt-css', get_template_directory_uri() . '/css/lite-yt-embed.min.css', array(), '1.0.0', 'all');
-    wp_enqueue_style('bs-datepicker-css', get_template_directory_uri() . '/css/bootstrap-datepicker3.standalone.min.css', array(), '1.0.0', 'all');
+    // wp_enqueue_style('lite-yt-css', get_template_directory_uri() . '/css/lite-yt-embed.min.css', array(), '1.0.0', 'all');
+    // wp_enqueue_style('bs-datepicker-css', get_template_directory_uri() . '/css/bootstrap-datepicker3.standalone.min.css', array(), '1.0.0', 'all');
     wp_enqueue_style('wwd-core-css', get_template_directory_uri() . '/css/style.min.css', array(), '0.0.4', 'all');
 
     //  I have moved away from this library
     // wp_enqueue_script('fitvids', get_stylesheet_directory_uri() . '/js/jquery.fitvids.js', array('jquery'), '1.0', true);
-    wp_enqueue_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', array(), '1.16.1', true);
+    // wp_enqueue_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', array(), '1.16.1', true);
     wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '4.5.0', true);
-    wp_enqueue_script('fitvids', get_stylesheet_directory_uri() . '/js/lite-yt-embed.min.js', array('jquery'), '1.0', true);
-    wp_enqueue_script('bs-datepicker', get_stylesheet_directory_uri() . '/js/bootstrap-datepicker.min.js', array('jquery', 'bootstrap'), '1.0', true);
-    wp_enqueue_script('bs-datepicker-locale', get_stylesheet_directory_uri() . '/js/bootstrap-datepicker.en-AU.min.js', array('jquery', 'bootstrap', 'bs-datepicker'), '1.0', true);
+    // wp_enqueue_script('fitvids', get_stylesheet_directory_uri() . '/js/lite-yt-embed.min.js', array('jquery'), '1.0', true);
+    // wp_enqueue_script('bs-datepicker', get_stylesheet_directory_uri() . '/js/bootstrap-datepicker.min.js', array('jquery', 'bootstrap'), '1.0', true);
+    // wp_enqueue_script('bs-datepicker-locale', get_stylesheet_directory_uri() . '/js/bootstrap-datepicker.en-AU.min.js', array('jquery', 'bootstrap', 'bs-datepicker'), '1.0', true);
     wp_enqueue_script('wwd-core-js', get_template_directory_uri() . '/js/core.min.js', array('jquery', 'bs-datepicker'), '0.0.4', true);
 }
 add_action('wp_enqueue_scripts', 'wwd_load_scripts');
--- app/public/wp-content/themes/hannahgracebeauty/template-parts/content-booking-calendar.php
+++ app/public/wp-content/themes/hannahgracebeauty/template-parts/content-booking-calendar.php
@@ -7,97 +7,121 @@
 <section class="booking-calendar-container">
 <h1>Booking Calendar</h1>
 <?php
+$time = date("Y-m-d", time());
+// var_dump(date('Y-m-d', strtotime($_POST['selectedTime'])));
+$selectedDate = 
+	isset($_POST['selectedTime']) ? 
+		date('Y-m-d', strtotime($_POST['selectedTime'])) : 
+			(isset($_POST['selectedDate']) ? 
+				$_POST['selectedDate'] : 
+					$time);
 
-function build_calendar($month, $year) {
-	$daysOfWeek = array('S','M','T','W','T','F','S');
-	$firstDayOfMonth = mktime(0,0,0,$month,1,$year);
-	$numberDays = date('t',$firstDayOfMonth);
-	$dateComponents = getdate($firstDayOfMonth);
-	$monthName = $dateComponents['month'];
-	$dayOfWeek = $dateComponents['wday'];
-	$calendar = "<table class='calendar table table-condensed table-bordered'>";
-	$calendar .= "<form method='GET'>";
-	$calendar .= "<caption>";
-	$calendar .= "<button type='submit' name='direction' value='-1' class='float-left'><<</button>";
-	$calendar .= $monthName. " " .$year;
-	$calendar .= "<button type='submit' name='direction' value='1' class='float-right'>>></button>";
-	$calendar .= "</caption>";
-	$calendar .= "<tr>";
-	foreach($daysOfWeek as $day) {
-		$calendar .= "<th class='header'>$day</th>";
-	}
-	$currentDay = 1;
-	$calendar .= "</tr><tr>";
-	if ($dayOfWeek > 0) {
-		$calendar .= "<td colspan='$dayOfWeek'>&nbsp;</td>";
-	}
-	$month = str_pad($month, 2, "0", STR_PAD_LEFT);
-	while($currentDay <= $numberDays){
-		if($dayOfWeek == 7){
-			$dayOfWeek = 0;
-			$calendar .= "</tr><tr>";
-		}
-		$currentDayRel = str_pad($currentDay, 2, "0", STR_PAD_LEFT);
-		$date = "$year-$month-$currentDayRel";
+// var_dump($selectedDate);
+$selectedTime = $_POST['selectedTime'];
+$dateparts = explode('-', $selectedDate);
+$month = $dateparts[1];
+$year = $dateparts[0];
 
-		// Is this today?
-		if(date('Y-m-d') == $date) {
-			$calendar .= "<td class='day bg-success' rel='$date'>";
-			$calendar .= "<button type='submit' value='" .$date. "' name='selectedDate' />" .$currentDay. "</button></td>";
-		} else {
-			$calendar .= "<td class='day' rel='$date'>$currentDay</td>";
-		}
+// $calendar = build_calendar($numMonth, $numYear);
+$daysOfWeek = array('S','M','T','W','T','F','S');
+$firstDayOfMonth = mktime(0,0,0,$month,1,$year);
+$numberDays = date('t',$firstDayOfMonth);
+$dateComponents = getdate($firstDayOfMonth);
+$monthName = $dateComponents['month'];
+$dayOfWeek = $dateComponents['wday'];
+$prev_month = date('Y-m-d', strtotime($selectedDate . ' - 1 month'));
+$next_month = date('Y-m-d', strtotime($selectedDate . ' + 1 month'));
 
-		$currentDay++;
-		$dayOfWeek++;
-	}
-	if($dayOfWeek != 7){
-		$remainingDays = 7 - $dayOfWeek;
-		$calendar .= "<td colspan='$remainingDays'>&nbsp;</td>";
-	}
-	$calendar .= "</tr>";
-	$calendar .= "</form>";
-	$calendar .= "</table>";
-	return $calendar;
+$calendar .= "<div class='calender-caption-container'>";
+$calendar .= "<div><button type='submit' name='selectedDate' value='" .$prev_month. "' class='float-left'><<</button></div>";
+$calendar .= "<div class='calendar-caption'>" .$monthName. " " .$year. "</div>";
+$calendar .= "<div><button type='submit' name='selectedDate' value='" .$next_month. "' class='float-right'>>></button></div>";
+$calendar .= "</div>";
+
+$calendar .= "<table class='booking-calendar table table-condensed'>";
+$calendar .= "<tr>";
+foreach($daysOfWeek as $day) {
+	$calendar .= "<th class='header'>$day</th>";
 }
-
-if (isset($_POST['back']) || isset($_POST['forward'])) {
-
-	if (isset($_POST['back'])) {
-
+$currentDay = 1;
+$calendar .= "</tr><tr>";
+if ($dayOfWeek > 0) {
+	$calendar .= "<td colspan='$dayOfWeek'>&nbsp;</td>";
+}
+$month = str_pad($month, 2, "0", STR_PAD_LEFT);
+while($currentDay <= $numberDays){
+	if($dayOfWeek == 7){
+		$dayOfWeek = 0;
+		$calendar .= "</tr><tr>";
 	}
-}
-$post_monthname = $_POST['monthname'];
-$post_year = $_POST['year'];
-$post_direction = $_POST['direction'];
-$post_selectedDate = $_POST['selectedDate'];
+	$currentDayRel = str_pad($currentDay, 2, "0", STR_PAD_LEFT);
+	$date = "$year-$month-$currentDayRel";
 
-$time = time();
-$numMonth = date('m', $time);
+	// Is this today?
+	if (date('Y-m-d') == $date) {
+		$calendar .= "<td class='day bg-success' rel='$date'>";
+	} else if ($selectedDate == $date) {
+		$calendar .= "<td class='day selected' rel='$date'>";
+	} else {
+		$calendar .= "<td class='day' rel='$date'>";
+	}
+	$calendar .= "<button type='submit' value='" .$date. "' name='selectedDate' />" .$currentDay. "</button></td>";
 
-if (!isset($post_month)) {
-	$post_month = date("F", mktime(0, 0, 0, $numMonth, 10));
+	$currentDay++;
+	$dayOfWeek++;
 }
-if (!isset($post_year)) {
-	$post_year = date('Y', $time);
+if($dayOfWeek != 7){
+	$remainingDays = 7 - $dayOfWeek;
+	$calendar .= "<td colspan='$remainingDays'>&nbsp;</td>";
 }
+$calendar .= "</tr>";
+$calendar .= "</table>";
+// echo $calendar;
 
-$calendar_date = mktime(0, 0, 0, (int)$post_month, 1, (int)$post_year);
-var_dump($calendar_date);
-if (isset($post_direction)) {
-	$calendar_date = strtotime($post_direction. " months", $calendar_date);
-}
-var_dump($calendar_date);
-// $time = mktime(0,0,0,$month,1,$year);
-
-$time = time();
-$numDay = date('d', $calendar_date);
-$numMonth = date('m', $calendar_date);
-$nameMonth = date("F", mktime(0, 0, 0, $numMonth, 10));
-$numYear = date('Y', $calendar_date);
-$calendar = build_calendar($numMonth, $numYear);
-echo $calendar;
 ?>
-<input type='text' value='<?php echo $nameMonth; ?>' name='monthname' />
-<input type='text' value='<?php echo $numYear; ?>' name='year' />
+<div class="calendar-container">
+	<div class="calendar">
+		<form method="GET">
+	<?php echo $calendar; ?>
+		</form>
+	</div>
+	<div class="calendar-times">
+<?php
+	echo selectTimesofDay('09:00', '17:59', '15 minutes', $selectedDate, $selectedTime);
+?>
+	</div>
+</div>
+
+<input type='text' value='<?php echo $selectedDate; ?>' name='booking-date' />
+<input type='text' value='<?php echo date('H:i', strtotime($_POST['selectedTime'])); ?>' name='booking-time' />
 </section>
+<?php
+function selectTimesofDay($start=false, $end=false, $interval='5 minutes', $selectedDate, $selectedTime){
+    $interval = DateInterval::createFromDateString($interval);
+    $rounding_interval = $interval->i * 60;
+    $date = new DateTime(
+        date('Y-m-d H:i', round(strtotime($selectedDate .' '. $start) / $rounding_interval) * $rounding_interval)
+    );
+    $end = new DateTime(
+        date('Y-m-d H:i', round(strtotime($selectedDate .' '. $end) / $rounding_interval) * $rounding_interval)
+    );
+
+    $opts = array();
+    while ($date < $end) {
+        $data = $date->format('g:i a');
+        $opts[] = '<button name="selectedTime" value="' .$date->format('Y-m-d H:i'). '">'.$data.'</button>';
+        $date->add($interval);
+    }
+
+    return count($opts) < 1 ? 
+        '<option value="-1">- closed -</option>' : 
+        implode("\n", $opts);
+}
+?>
+<script>
+	(function($) {
+		$("button[name=selectedDate").on('click', function(e) {
+			$("input[name=booking-date]").val($(this).val());
+		})
+	})(jQuery)
+</script>
