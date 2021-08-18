6298e731d7aec29034863156aa5d24ca10e8aa8a
Woolston Web Design <steven.woolston@gmail.com>
2021-08-18 10:12:02 +1000

    Google analytics

--- app/public/wp-content/themes/hannahgracebeauty/header.php
+++ app/public/wp-content/themes/hannahgracebeauty/header.php
@@ -13,6 +13,14 @@
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 <?php endif; ?>
     <?php wp_head(); ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-204686943-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-204686943-1');
+    </script>
 </head>
 <body <?php body_class(); ?>>
 <header class="page-header">
